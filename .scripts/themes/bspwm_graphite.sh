53efb1b46dd483e36bc61d132e067772a6f721f5
Wallkerock <lluboopecienka@gmail.com>
2019-12-04 21:42:10 +0100

    X-setup

--- .scripts/themes/bspwm_graphite.sh
+++ .scripts/themes/bspwm_graphite.sh
@@ -13,22 +13,29 @@ cp ~/.config/bspwm/Bspwm-Graphite/JSON.sublime-settings ~/.config/sublime-text-2
 cp ~/.config/bspwm/Bspwm-Graphite/.Xresources /home/lubomir/ &
 cp ~/.config/bspwm/Bspwm-Graphite/.vimrc /home/lubomir/ &
 cp ~/.config/bspwm/Bspwm-Graphite/ROFI.rasi /home/lubomir/.config/rofi/themes/ &
-cp ~/.config/bspwm/Bspwm-Graphite/compton.conf /home/lubomir/.config/ &
+cp ~/.config/bspwm/Bspwm-Graphite/picom.conf /home/lubomir/.config/ &
 cp ~/.config/bspwm/Bspwm-Graphite/dunstrc /home/lubomir/.config/dunst/ &
 cp ~/.config/bspwm/Bspwm-Graphite/zathurarc /home/lubomir/.config/zathura/ &
 cp ~/.config/bspwm/Bspwm-Graphite/termite/config /home/lubomir/.config/termite/ &
 
 sed -i 's/iconset=.*/iconset=Papirus/g' ~/.config/smplayer/smplayer.ini &
 
+#feh --bg-fill /home/lubomir/Pictures/backgrounds/bspwm-graphite/bspwm-logo.png &
+feh --bg-fill /home/lubomir/Pictures/backgrounds/bspwm-graphite/minimalist.jpg &
+
 xfconf-query -c xsettings -p /Net/ThemeName -s "Stylish-Light-Laptop" &
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Limon" &
 
 bspc config border_width         3
-bspc config window_gap           26
+bspc config window_gap           16
 
-bspc config focused_border_color            "#595959"
+bspc config borderless_monocle   false
+bspc config gapless_monocle      true
+bspc config single_monocle       false
+
+bspc config focused_border_color            "#444444"
 bspc config normal_border_color             "#D6D6D6"
-bspc config presel_feedback_color           "#595959"
+bspc config presel_feedback_color           "#444444"
 bspc config window_gap                      "#bspwm_window_gap"
 
 bspc config top_padding         25
@@ -37,9 +44,8 @@ bspc config bottom_padding      0
 bspc config left_padding        0
 
 sleep 1; xrdb -load ~/.Xresources &
-feh --bg-fill /home/lubomir/Pictures/backgrounds/bspwm-logo.png &
 #killall -q polybar &
 #pulseaudio -k &
 killall sxhkd && sleep 1; sxhkd &
-killall compton && sleep 1; compton &
+killall picom && sleep 1; picom &
 killall dunst; notify-send "restart dunst" &
